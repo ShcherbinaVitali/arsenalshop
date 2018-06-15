c2e9123d2acb585d95bd4f9434d426bbf866b2ad
ShcherbinaVitali <shcherbinavitali@gmail.com>
2018-06-15 11:31:46 +0300

    Changed CDN css and js files to local

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -5,15 +5,16 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+		<link rel="stylesheet" href="/css/vendor/bootstrap-4.0.0/bootstrap.min.css">
 		
 		<link rel="stylesheet" href="/css/style.css">
 		
 		<script src="/js/vendor/jquery-3.3.1.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 		
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+		<script src="/js/vendor/bootstrap-4.0.0/bootstrap.min.js"></script>
+		
+		<link rel="stylesheet" href="/css/vendor/fontawesome/fontawesome-all.min.css">
 		
 		@yield('header.styles')
 		
@@ -32,11 +33,9 @@
 				@php
 					$urlPath = request()->path();
 					$cat     = strpos($urlPath, 'catalog');
-					$prod    = strpos($urlPath, 'product');
 					
 					if (
 						$cat === false
-						&& $prod === false
 					) {
 						session()->forget('category.active');
 					}
