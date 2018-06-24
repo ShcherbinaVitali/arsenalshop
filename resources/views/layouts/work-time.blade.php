0ce865b13727f3db304083b2d959e6c04e50006e
ShcherbinaVitali <shcherbinavitali@gmail.com>
2018-06-24 19:02:19 +0300

    Added work-time block to head

---
+++ resources/views/layouts/work-time.blade.php
@@ -0,0 +1,11 @@
+<div class="work-hours-wrap">
+	<h4 class="hours-title">
+		@lang('Режим работы'):
+	</h4>
+	<div class="hours-item">
+		Пн-Пт: 8.30-17.00 (без обеда)
+	</div>
+	<div class="hours-item">
+		Сб, Вс: выходной
+	</div>
+</div>
